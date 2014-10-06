b7ff0dc4f4bf963a9e95dc75bff62925a04e7fec
JP Hastings-Spital <jphastings@blinkbox.com>
2014-10-06 16:17:23 +0100

    TicToc functionality

---
+++ lib/blinkbox/tictoc.rb
@@ -0,0 +1,36 @@
+module Blinkbox
+  module CommonHelpers
+    # Gives you two methods, `tic` and `toc`. Without arguments `toc` will give you the number of milliseconds
+    # since you last called `tic`. Takes one argument, which can be used to have two timers running independently:
+    #
+    # @example: Basic use
+    #   include Blinkbox::CommonHelpers::TicToc
+    #   tic
+    #   sleep 9
+    #   p toc
+    #   # => 9001
+    #   # (It is over 9000 because time is an illusion, lunchtime doubly so)
+    #
+    # @example: Parallel use
+    #   include Blinkbox::CommonHelpers::TicToc
+    #   tic :a
+    #   sleep 1
+    #   tic :b
+    #   sleep 1
+    #   p toc :b
+    #   # => 1000
+    #   sleep 1
+    #   p toc :a
+    #   # => 3000
+    module TicToc
+      def tic(label = :default)
+        (@@timers ||= {})[label] = Time.now.utc.to_r
+      end
+
+      def toc(label = :default)
+        return nil if @@timers[label].nil?
+        ((Time.now.utc.to_r - @@timers[label]) * 1000).to_f
+      end
+    end
+  end
+end
